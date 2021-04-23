3004446203d34ca69e002d33c9515f747811c8f2
Patrick McGrath <patrickmcgrath29@gmail.com>
2021-04-23 15:57:57 -0400

    Some updates

---
+++ src/api.py
@@ -0,0 +1,13 @@
+def create(event, context):
+    print("create")
+    print(event)
+
+
+def get(event, context):
+    print("get")
+    print(event)
+
+
+def delete(event, context):
+    print("delete")
+    print(event)
